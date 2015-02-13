65ab092ed039c11cd37110ade7d63020aa8b2415
tkawajir <taka.kawajiri@gmail.com>
2015-02-14 08:40:19 +0900

    Add support LGL24

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/lge/vs985/full_vs985.mk)
+$(call inherit-product, device/lge/lgl24/full_lgl24.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -6,12 +6,10 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := cm_vs985
+PRODUCT_NAME := cm_lgl24
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
-    PRODUCT_NAME="g3_vzw_us" \
-    BUILD_FINGERPRINT="lge/g3_vzw/g3:4.4.2/KVT49L.VS98512B/VS98512B.1414669625:user/release-keys" \
-    PRIVATE_BUILD_DESC="g3_vzw-user 4.4.2 KVT49L.VS98512B VS98512B.1414669625 release-keys"
-
-PRODUCT_GMS_CLIENTID_BASE := android-verizon
+    PRODUCT_NAME="g3_kddi_jp" \
+    BUILD_FINGERPRINT="KDDI/g3_kddi_jp/g3:4.4.2/KVT49L.LGL2410a/LGL2410a.1403186587:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_kddi_jp-user 4.4.2 KVT49L.LGL2410a LGL2410a.1403186587 release-keys"
